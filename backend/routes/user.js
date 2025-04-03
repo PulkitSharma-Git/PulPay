2a4508eb841046060200f5a8819c9e3b7d7dd7d0
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-04-04 00:59:39 +0530

    Sisn Up endpoint done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,48 +1,58 @@
 import express from "express";
 import {z} from "zod";
-import { User } from "./../db.js"
-
-
-
+import { User } from "./../db.js";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config.js";
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-    const email = req.body.email;
-    const password  = req.body.password;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
+const userSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    firstName: z.string().min(1),
+    lastName: z.string().min(1)
+})
 
-    const User = {
-        email: email,
-        password: password,
-        firstName: firstName,
-        lastName: lastName
-    }
-    
+router.post("/signup", async (req, res) => {
+
     //Validation using zod
-    const userSchema = z.object({
-        email: z.string().email(),
-        password: z.string().min(6),
-        firstName: z.string().min(1),
-        lastName: z.string().min(1)
-    })
     const result =  userSchema.safeParse(User);
     if (!result.success) {
-        return res.status(400).json({
-            message: "Validation failed",
-            errors: result.error.errors
+        return res.status(411).json({
+            message: "Invalid Inputs"
+        })
+    }
+    
+    const existingUser = await User.findOne({
+        email: req.body.email
+    });
+
+    if(existingUser) {
+        res.status(411).json({
+            message: "User already exist. Please Sign Up"
         })
     }
 
+    const User = {
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password
+    }
 
 
     //Db call to save the user
-    User.create(User);
+    const user = User.create(User);
+
+    const token = jwt.sign({
+        userId: user._id
+    }, JWT_SECRET)
 
 
     res.status(201).json({
-        message: "User Signed up successfully"
+        message: "User created Sccessfully",
+        token: token
+
     })
 })
 
@@ -51,7 +61,8 @@ router.post("signin", (req, res) => {
     const email = req.body.email;
     const password  = req.body.password;
 
-    
+
+
 
 })
 
